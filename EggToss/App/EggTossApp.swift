592a1547cd668996d63bc97b450ee787bf121ade
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-28 15:03:00 +0200

    init

---
+++ EggToss/App/EggTossApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTossApp.swift
+//  EggToss
+//
+//  Created by Serhii Babchuk on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggTossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ETLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-EggToss/Pods-EggToss-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EggToss : NSObject
+@end
+@implementation PodsDummy_Pods_EggToss
+@end
